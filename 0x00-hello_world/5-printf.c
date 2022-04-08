fcf396fadb4e68ad6408f3cdc9190b0791a16e7f
Khadijat Suleiman <deejahsul@gmail.com>
2022-04-08 06:25:14 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
 /**
  * main - The containing function of the programme
  * Return: 0 if sucessful
- */ 
-int main (void)
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
