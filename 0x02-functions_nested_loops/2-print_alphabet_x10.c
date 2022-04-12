8909b321f5f0eb15ea7231e254ed5a9cfc0bd8ae
Khadijat Suleiman <deejahsul@gmail.com>
2022-04-12 15:09:37 -0700

    g

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - counts the alphabet 10 times
+ */
+int main(void)
+{
+	int i
+	char j
+	for (i = 0; i<10; j++)
+	{
+		(j = 'a'; j <= 'z'; j++)
+		{
+			putchar(j);
+		}
+		putchar('\n')
+	}
+}
