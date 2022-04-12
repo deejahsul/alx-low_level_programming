cd162daee37bb4de5a0d1e55d3a67afb65703530
Khadijat Suleiman <deejahsul@gmail.com>
2022-04-12 15:25:25 -0700

    g

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,16 +3,13 @@
 /**
  * main - counts the alphabet 10 times
  */
+
 int main(void)
 {
 	int i
 	char j
 	for (i = 0; i<10; j++)
-	{
 		(j = 'a'; j <= 'z'; j++)
-		{
 			putchar(j);
-		}
 		putchar('\n')
-	}
 }
