7fcec0155543a16267dc5a29e6b41ad6ad6f0eb8
Khadijat Suleiman <deejahsul@gmail.com>
2022-04-12 15:41:39 -0700

    g

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,8 +7,15 @@
 void print_alphabet(void)
 {
 	char i;
-
-	for (i = 'a'; i <= 'z'; i++)
-		putchar(i);
+	putchar('i');
 	putchar('\n');
+}
+/**
+ * main -main function
+ * Return; always 0
+ */
+int main(void)
+{
+	print_alphabet(;)
+	return (0);
 }
