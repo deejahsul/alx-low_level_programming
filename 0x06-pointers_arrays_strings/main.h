e31952694ddb051b09af00ba8ae49ba3f7299c75
Khadijat Suleiman <deejahsul@gmail.com>
2022-04-24 09:46:21 -0700

    hello 2

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,34 +1,10 @@
-#include "main.h"
-
-/**
- * print_number - prints integer
- * @n: param
- */
-void print_number(int n)
-{
-	unsigned int i, j, count;
-
-	if (n < 0)
-	{
-		_putchar(45);
-		i = n * -1;
-	}
-	else
-	{
-		i = n;
-	}
-
-	d = i;
-	count = 1;
-
-	while (j > 9)
-	{
-		j /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((i / count) % 10) + 48);
-	}
-}
+void print_number(int n);
+char *rot13(char *);
+char *leet(char *);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
