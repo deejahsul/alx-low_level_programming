4d74778b7eece3a0e79c63e55c5904fc82bdb5d0
Khadijat Suleiman <deejahsul@gmail.com>
2022-04-25 02:29:45 -0700

    hello6

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,14 +2,14 @@
 
 /**
  * main - containing funct
- * Return: 0 if successful
+ * Return: 0 when successfull
  */
 
 int main(void)
 {
-	int i;
+	char i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 'z'; i >= 'a'; i--)
 	{
 		putchar(i);
 	}
